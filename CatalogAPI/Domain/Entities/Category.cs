6ff81452d4a701a283580bee3989c70e4366210b
Pedro <pedro_alv12@hotmail.com>
2025-07-12 16:55:02 -0300

    Inital Commit

---
+++ CatalogAPI/Domain/Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace APICatalogo.Domain.Entities
+{
+    public class Category
+    {
+        public int CategoryId { get; private set; }
+        public string? ImageUrl { get; set; }
+        public string? Name { get; set; }
+    }
+}
---
+++ CatalogAPI/Domain/Entities/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace APICatalogo.Domain.Entities
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public float Price { get; set; }
+        public string? ImageUrl { get; set; }
+        public float Stock { get; set; }
+        public DateTime RegistrationDate { get; set; }
+    }
+}
